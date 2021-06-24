f92cd3e609ac2d2ffa4d4e0cd29acae7f083cf25
Nils Müller <shimst3r@gmail.com>
2021-06-24 17:17:40 +0200

    Initial prototype

---
+++ pysofa/__init__.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2021 shimst3r
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+pysofa computes the Sepsis-related Organ Failure Assessment (SOFA) score
+according to Singer et al.:
+    https://doi.org/10.1001%2Fjama.2016.0287
+"""
+from collections import namedtuple
+from typing import Optional
+
+__version__ = "1.0.0"
+
+
+Catecholamine = namedtuple("Catecholamine", ["name", "dosage"])
+Condition = namedtuple(
+    "Condition",
+    [
+        "mean_arterial_pressure",
+        "catecholamine",
+        "platelets_count",
+        "creatinine_level",
+        "bilirubin_level",
+        "glasgow_coma_scale",
+        "partial_pressure_of_oxygen",
+        "is_mechanically_ventilated",
+    ],
+)
+
+
+def compute_score(condition: Condition) -> int:
+    cvs_score = compute_score_for_cardiovascular_system(
+        mean_arterial_pressure=condition.mean_arterial_pressure,
+        catecholamine=condition.catecholamine,
+    )
+    cg_score = compute_score_for_coagulation(platelets_count=condition.platelets_count)
+    kdny_score = compute_score_for_kidneys(creatinine_level=condition.creatinine_level)
+    livr_score = compute_score_for_liver(bilirubin_level=condition.bilirubin_level)
+    ns_score = compute_score_for_nervous_system(
+        glasgow_coma_scale=condition.glasgow_coma_scale
+    )
+    rs_score = compute_score_for_respiratory_system(
+        partial_pressure_of_oxygen=condition.partial_pressure_of_oxygen,
+        is_mechanically_ventilated=condition.is_mechanically_ventilated,
+    )
+    return cvs_score + cg_score + kdny_score + livr_score + ns_score + rs_score
+
+
+def compute_score_for_cardiovascular_system(
+    mean_arterial_pressure: float, catecholamine: Optional[Catecholamine]
+) -> int:
+    """
+    Computes score based on mean arterial pressure or catecholamine therapy.
+    """
+    if catecholamine:
+        if catecholamine.name == "dopamine":
+            if catecholamine.dosage <= 5:
+                return 2
+            if catecholamine.dosage < 15:
+                return 3
+            return 4
+        if catecholamine.name == "dobutamine":
+            return 2
+        if catecholamine.name in {"epinephrine", "norepinephrine"}:
+            if catecholamine.dosage <= 0.1:
+                return 3
+            return 4
+    if mean_arterial_pressure < 70:
+        return 1
+    return 0
+
+
+def compute_score_for_coagulation(platelets_count: int) -> int:
+    """
+    Computes score based on platelets count (unit is number per microliter).
+    """
+    if platelets_count < 20_000:
+        return 4
+    if platelets_count < 50_000:
+        return 3
+    if platelets_count < 100_000:
+        return 2
+    if platelets_count < 150_000:
+        return 1
+    return 0
+
+
+def compute_score_for_kidneys(creatinine_level: float) -> int:
+    """Computes score based on Creatinine level (unit is mg/dl)."""
+    if creatinine_level >= 5.0:
+        return 4
+    if creatinine_level >= 3.5:
+        return 3
+    if creatinine_level >= 2.0:
+        return 2
+    if creatinine_level >= 1.2:
+        return 1
+    return 0
+
+
+def compute_score_for_liver(bilirubin_level: float) -> int:
+    """Computes score based on Bilirubin level (unit is mg/dl)."""
+    if bilirubin_level >= 12.0:
+        return 4
+    if bilirubin_level >= 6.0:
+        return 3
+    if bilirubin_level >= 2.0:
+        return 2
+    if bilirubin_level >= 1.2:
+        return 1
+    return 0
+
+
+def compute_score_for_nervous_system(glasgow_coma_scale: int) -> int:
+    """
+    Computes score based on Glasgow Coma Scale, see paper by Teasdale et al.:
+        https://doi.org/10.1016/S0140-6736(74)91639-0
+    """
+    if glasgow_coma_scale < 6:
+        return 4
+    if glasgow_coma_scale < 10:
+        return 3
+    if glasgow_coma_scale < 13:
+        return 2
+    if glasgow_coma_scale < 15:
+        return 1
+    return 0
+
+
+def compute_score_for_respiratory_system(
+    partial_pressure_of_oxygen: float, is_mechanically_ventilated: bool
+) -> int:
+    """Computes score based on PaO2 (unit is mmHg)."""
+    if partial_pressure_of_oxygen < 100 and is_mechanically_ventilated:
+        return 4
+    if partial_pressure_of_oxygen < 200 and is_mechanically_ventilated:
+        return 3
+    if partial_pressure_of_oxygen < 300:
+        return 2
+    if partial_pressure_of_oxygen < 400:
+        return 1
+    return 0
---
+++ tests/test_pysofa.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+import pysofa
+from pysofa import Catecholamine, Condition
+
+
+def test_version():
+    assert pysofa.__version__ == "1.0.0"
+
+
+@pytest.mark.parametrize(
+    "condition,score",
+    [
+        (Condition(70, None, 150_000, 1.1, 1.1, 15, 450, False), 0),
+        (
+            Condition(
+                50, Catecholamine("epinephrine", 0.5), 100_000, 3.3, 6.1, 7, 150, True
+            ),
+            16,
+        ),
+        (
+            Condition(
+                50, Catecholamine("dobutamine", 1), 90_000, 2.3, 4.1, 10, 250, False
+            ),
+            12,
+        ),
+    ],
+)
+def test_compute_score(condition, score):
+    assert score == pysofa.compute_score(condition=condition)
+
+
+@pytest.mark.parametrize(
+    "map,catecholamine,score",
+    [
+        (50, Catecholamine("dopamine", 5), 2),
+        (50, Catecholamine("dopamine", 10), 3),
+        (50, Catecholamine("dopamine", 20), 4),
+        (50, Catecholamine("dobutamine", 1), 2),
+        (50, Catecholamine("epinephrine", 0.1), 3),
+        (50, Catecholamine("epinephrine", 0.5), 4),
+        (50, Catecholamine("norepinephrine", 0.1), 3),
+        (50, Catecholamine("norepinephrine", 0.5), 4),
+        (50, None, 1),
+        (70, None, 0),
+    ],
+)
+def test_compute_score_for_cardiovascular_system(map, catecholamine, score):
+    assert score == pysofa.compute_score_for_cardiovascular_system(
+        mean_arterial_pressure=map, catecholamine=catecholamine
+    )
+
+
+@pytest.mark.parametrize(
+    "platelets,score",
+    [
+        (200_000, 0),
+        (150_000, 0),
+        (149_000, 1),
+        (100_000, 1),
+        (99_000, 2),
+        (50_000, 2),
+        (49_000, 3),
+        (20_000, 3),
+        (19_000, 4),
+        (1_000, 4),
+    ],
+)
+def test_compute_score_for_coagulation(platelets, score):
+    assert score == pysofa.compute_score_for_coagulation(platelets_count=platelets)
+
+
+@pytest.mark.parametrize(
+    "creatinine,score",
+    [
+        (0.0, 0),
+        (1.1, 0),
+        (1.2, 1),
+        (1.9, 1),
+        (2.0, 2),
+        (3.4, 2),
+        (3.5, 3),
+        (4.9, 3),
+        (5.0, 4),
+        (7.0, 4),
+    ],
+)
+def test_compute_score_for_kidneys(creatinine, score):
+    assert score == pysofa.compute_score_for_kidneys(creatinine_level=creatinine)
+
+
+@pytest.mark.parametrize(
+    "bilirubin,score",
+    [
+        (0.0, 0),
+        (1.1, 0),
+        (1.2, 1),
+        (1.9, 1),
+        (2.0, 2),
+        (5.9, 2),
+        (6.0, 3),
+        (11.9, 3),
+        (12.0, 4),
+        (42.0, 4),
+    ],
+)
+def test_compute_score_for_liver(bilirubin, score):
+    assert score == pysofa.compute_score_for_liver(bilirubin_level=bilirubin)
+
+
+@pytest.mark.parametrize(
+    "coma_scale,score",
+    [
+        (15, 0),
+        (14, 1),
+        (13, 1),
+        (12, 2),
+        (11, 2),
+        (10, 2),
+        (9, 3),
+        (8, 3),
+        (7, 3),
+        (6, 3),
+        (5, 4),
+        (4, 4),
+        (3, 4),
+        (2, 4),
+        (1, 4),
+        (0, 4),
+    ],
+)
+def test_compute_score_for_nervous_system(coma_scale, score):
+    assert score == pysofa.compute_score_for_nervous_system(
+        glasgow_coma_scale=coma_scale
+    )
+
+
+@pytest.mark.parametrize(
+    "pressure,ventilation,score",
+    [
+        (50, False, 2),
+        (50, True, 4),
+        (150, False, 2),
+        (150, True, 3),
+        (250, False, 2),
+        (250, True, 2),
+        (350, False, 1),
+        (350, True, 1),
+        (450, False, 0),
+        (450, True, 0),
+    ],
+)
+def test_compute_score_for_respiratory_system(pressure, ventilation, score):
+    assert score == pysofa.compute_score_for_respiratory_system(
+        partial_pressure_of_oxygen=pressure, is_mechanically_ventilated=ventilation
+    )
